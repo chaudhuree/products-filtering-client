b0074acd3bf5ba2722040f245a85ad7de1897bb3
chaudhuree <chaudhuree00@gmail.com>
2024-08-17 15:15:04 +0600

    added all required variable in the Products context

--- src/context/Products.jsx
+++ src/context/Products.jsx
@@ -4,7 +4,10 @@ export default function ProductsProvider({ children }) {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [search, setSearch] = useState("");
-
+  const [brand, setBrand] = useState("");
+  const [category, setCategory] = useState("");
+  const [sort, setSort] = useState("priceAsc");
+  const [priceRange, setPriceRange] = useState([0, 4]);
 
   // all variables and functions that you want to share with the rest of the application should be placed in the context
   const contextValue = {
@@ -12,6 +15,14 @@ export default function ProductsProvider({ children }) {
     loading,
     search,
     setSearch,
+    brand,
+    setBrand,
+    category,
+    setCategory,
+    sort,
+    setSort,
+    priceRange,
+    setPriceRange,
   };
 
   return (
